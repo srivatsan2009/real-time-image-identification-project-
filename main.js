74c9c8752242b44367e9ae3375b897d3f3ff3d2d
srivatsan2009 <71771607+srivatsan2009@users.noreply.github.com>
2020-12-08 20:16:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+
+}
+
+function setup() {
+canvas=createCanvas(300,300);
+canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+} 
+
+function draw() {
+image(video,0,0,300,300);
+}
